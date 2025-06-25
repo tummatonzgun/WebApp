aaca11c4feb44570cf157701ee653652290f5648
tummaton <tummaton2559@gmail.com>
2025-06-25 08:22:31 +0700

    port 8080

--- Webapp/python-executor-app/src/app.py
+++ Webapp/python-executor-app/src/app.py
@@ -70,4 +70,4 @@ def result():
 
 if __name__ == "__main__":
     ip= socket.gethostbyname(socket.gethostname())
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8080)
